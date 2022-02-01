eb02344ed5fd9f03236fb9d1b4461045fd099507
David Clark <david.clark.1043@gmail.com>
2022-02-01 11:02:16 -0600

    Added Needs Comments in Database

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,60 @@
 const database = {
-    transientState: {}
+    // governors
+
+    // colonies
+
+    // facilities
+
+    // minerals
+
+    // facilityMinerals
+
+    // colonyMinerals
+
+    transientState: {
+        // selectedFacility
+        // selectedGovernor
+        // selectedMineral
+    }
 }
 
+// Get Governor Names Function
+
+// Set Active Governer Function
+
+
+// Get Colony Names Function
+
+// Get Minerals List
+
+// Get Mineral amount from facilityMinerals given Facility
+
+// Get Mineral amount from colonyMinerals given Colony
+
+
+// Get Transient State
+
+
+// Set Mineral amount from facilityMinerals given Facility
+
+// Set Mineral amount from colonyMinerals given Colony
+
+
+// Set Facility
 export const setFacility = (facilityId) => {
     database.transientState.selectedFacility = facilityId
     document.dispatchEvent( new CustomEvent("stateChanged") )
 }
 
+// Get Facilities
 export const getFacilities = () => {
     return database.facilities.map(f => ({...f}))
 }
 
+// Purchase Minerals and update state
 export const purchaseMineral = () => {
 
         // Broadcast custom event to entire documement so that the
         // application can re-render and update state
         document.dispatchEvent( new CustomEvent("stateChanged") )
-    }
 }
