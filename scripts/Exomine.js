e67f95b9a313fd36c2dd630fa45cfcd50de07a9c
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-01 15:02:51 -0600

    Filled out the html splitting up the sections

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,3 +1,36 @@
 // import htmlString functions from .js
 
 // export function that returns a string of html
+
+export const MiningMarketplace = () => {
+    return `
+        <header class="header">
+            <h1 class="title">Solar System Mining Marketplace</h1>
+        </header>
+        <article class="choices">
+            <section class="choices__governor">
+                
+            </section>
+            <section class="choices__facility">
+
+            </section>
+        </article>
+
+        <article class="facilityInventory>
+            <h2>Europa Minerals</h2>
+        </article>
+
+        <article class="facilityMinerals>
+            <h3>Facilitiy Minerals for Ganymede</h3>
+        </article>
+
+        <article>
+            <button id="purchase">Purchase Combo</button>
+        </article>
+
+        <article class="customerOrders">
+            <h2>Monthly Sales</h2>
+        </article>
+
+    `
+}
