640b15454e7638330f967b606df08605679b09f6
David Clark <david.clark.1043@gmail.com>
2022-02-01 13:57:29 -0600

    Added function needs to Governors.js

---
+++ scripts/governors/Governors.js
@@ -0,0 +1,5 @@
+// import getGovernor, setGovernor, getTransientState
+
+// export string containing dropdown menu containing only active governers
+
+// add an eventListener that invokes setGovernor
