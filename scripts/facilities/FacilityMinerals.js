c48aa8c4722058af938f55966638db3af3638af4
Randall Thomas <randallthomasmusic@gmail.com>
2022-02-01 11:30:21 -0800

    added extra module, and psudo code to FacilityMinerals module

---
+++ scripts/facilities/FacilityMinerals.js
@@ -0,0 +1,22 @@
+// import getFacilities, getMinerals, getFacilityMinerals, 
+// setMineral, 
+
+// Create a function that iterates through minerals
+
+// Find which facility is active 
+
+// Filter minerals at active facility
+
+// Check if quantity is more than 0
+
+// Add to array
+
+// Return string of array using join
+
+
+
+//  Add event listener for radio button state change
+
+// Calls setMineral and sets it
+
+
