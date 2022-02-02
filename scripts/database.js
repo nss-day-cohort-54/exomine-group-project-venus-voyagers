8712cba3368be9d8808144b275a071ec1152610e
David Clark <david.clark.1043@gmail.com>
2022-02-02 13:41:33 -0600

    Fix merge conflict

--- scripts/database.js
+++ scripts/database.js
@@ -55,13 +55,8 @@ const database = {
     ],
     transientState: {
         // Can uncomment these for testing purposes
-<<<<<<< HEAD
-        // selectedFacility: 1
-        // selectedGovernor: 1,
-=======
         // selectedFacility: 1,
         selectedGovernor: 3,
->>>>>>> main
         // selectedMineral: 1
     }
 }
