3baa83e739b7ee0c86d3fdcdee6d62bfd0eaf72b
Randall Thomas <randallthomasmusic@gmail.com>
2022-02-02 07:59:12 -0800

    SelectFacilities comments and changes

--- scripts/facilities/SelectFacility.js
+++ scripts/facilities/SelectFacility.js
@@ -2,31 +2,37 @@
 import { getFacilities, setFacility, getTransientState} from "../database.js";
 // export string containing dropdown menu containing only active facilities and only after a governor has been selected
 export const FacilitiesHTML = () => {
-
+// get state and governor list
     const state = getTransientState();
     const facilities = getFacilities();
-    const governorList = `<select name="governors" id="gov">`
-
+    // start drop down html select tag
+    const facilityList = `<select name="facilities">`
+    // check to make sure state of governorId is not undefined (option has been selected)
     if (state.governorId != undefined) {
+        // map out facilities which meet criteria
         const facilityFound = facilities.map(facility => {
+            // check if facility is active
             if (facility.active === true) {
-                governorList += `
-                <option value="${governor.id}">${governor.name}</option>`
+                // add options to facilityList
+                facilityList += `
+                <option value="${facility.id}">${facility.name}</option>`
             }
         })
-        governorList += "</select>"
+        facilityList += "</select>"
     }
     return governorList
 }
 // add an eventListener that invokes setFacility
 
 
-document.querySelector(
+document.addEventListener(
     "change",
     (event) => {
-        if (event.target.value === `${governor.id}`) {
+        if (event.target.value === `${facility.id}`) {
             setFacility(parseInt(event.target.value))
             document.dispatchEvent(new CustomEvent("stateChanged"))
         }
     }
 )
+
+console.log()
