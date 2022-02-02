90d0741c8d3e92f6f83e5d8731763583d8f813ea
David Clark <david.clark.1043@gmail.com>
2022-02-02 10:12:31 -0600

    Added initial html rendering

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
 // import exomine html function
+import { MiningMarketplace } from "./Exomine.js";
 
 // define variable using querySelector 
+const mainContainer = document.querySelector("#container")
 
 // call exomine html function
 
+const renderAllHTML = () => {
+    mainContainer.innerHTML = MiningMarketplace()
+}
+
+renderAllHTML()
+
 // add eventListener for statechanged that reloads the page
+
+document.addEventListener("stateChanged", event => {
+    renderAllHTML()
+})
